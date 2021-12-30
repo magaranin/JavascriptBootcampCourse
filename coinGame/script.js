2c5a6f282116a5c82e59d82d83dd37aac5845dd0
Margarita Sirbu <margaritapetru@hotmail.com>
2021-12-29 19:00:37 -0800

    Coin Game Demo

---
+++ coinGame/script.js
@@ -0,0 +1,56 @@
+function isTouching(a, b) {
+	const aRect = a.getBoundingClientRect();
+	const bRect = b.getBoundingClientRect();
+
+	return !(
+		aRect.top + aRect.height < bRect.top ||
+		aRect.top > bRect.top + bRect.height ||
+		aRect.left + aRect.width < bRect.left ||
+		aRect.left > bRect.left + bRect.width
+	);
+}
+
+const avatar = document.querySelector('#player');
+const coin = document.querySelector('#coin');
+
+window.addEventListener('keyup', function(event) {
+	if (event.key === 'ArrowDown' || event.key === 'Down') {
+		moveVertical(avatar, +50);
+	}
+	else if (event.key === 'ArrowUp' || event.key === 'Up') {
+		moveVertical(avatar, -50);
+	}
+	else if (event.key === 'ArrowRight' || event.key === 'Right') {
+		moveHorizontal(avatar, 50)
+		avatar.style.transform = 'scale(1, 1)'
+	}
+	else if (event.key === 'ArrowLeft' || event.key === 'Left') {
+		moveHorizontal(avatar, -50)
+		avatar.style.transform = 'scale(-1, 1)'
+	}
+	if (isTouching(avatar, coin)) moveCoin();
+});
+
+const moveVertical = (element, amount) => {
+	const currTop = extractPos(element.style.top);
+	element.style.top = `${currTop + amount}px`;
+};
+
+const moveHorizontal = (element, amount) => {
+	const currLeft = extractPos(element.style.left);
+	element.style.left = `${currLeft + amount}px`;
+}
+
+const extractPos = (pos) => {
+	if (!pos) return 100;
+	return parseInt(pos.slice(0, -2));
+};
+
+const moveCoin = () => {
+	const height = Math.floor(Math.random() * window.innerHeight);
+	const width = Math.floor(Math.random() * window.innerWidth);
+	coin.style.top = `${height}px`;
+	coin.style.top = `${width}px`;
+}
+
+moveCoin();
